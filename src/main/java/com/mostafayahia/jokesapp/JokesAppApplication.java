7ab595f74058176cf5b9331cbb2cada6e3096dea
MostafaYahia <mostafayehyax23@gmail.com>
2020-07-09 14:08:33 +0200

    Refactor to use XML based configuration

--- src/main/java/com/mostafayahia/jokesapp/JokesAppApplication.java
+++ src/main/java/com/mostafayahia/jokesapp/JokesAppApplication.java
@@ -2,8 +2,10 @@ package com.mostafayahia.jokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:chuck-norris-configuration.xml")
 public class JokesAppApplication {
 
     public static void main(String[] args) {
